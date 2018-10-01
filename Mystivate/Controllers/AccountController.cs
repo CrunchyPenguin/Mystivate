9bae5f52383db4ab8d719b284481f58f3c2b8023
Ruben Hemmes <rubenhemmes@live.nl>
2018-10-01 12:52:16 +0200

    Initial commit

---
+++ Mystivate/Controllers/AccountController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Mystivate.Models;
+
+namespace Mystivate.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Login(LoginViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+
+            }
+            return RedirectToAction("Info", "Home");
+        }
+    }
+}
